b4b005b518928b12343a99220a0e66a6f3604c66
Simon Davy <simon.davy@thedatalab.org>
2023-05-24 18:00:49 +0000

    Fix some issues with the license update tooling

--- scripts/renew-license.sh
+++ scripts/renew-license.sh
@@ -2,6 +2,9 @@
 set -euo pipefail
 apt update --yes
 apt install expect --yes --no-install-recommends
+# stinit expects a binary called stata, but we only ship the stata-mp binary to save space
+# so fake it
+ln -s /usr/local/stata/stata-mp /usr/local/stata/stata
 # run expect script to feed input to stinit
 /src/scripts/stinit.exp "$1" "$2" "$3"
 # copy resulting license file out
